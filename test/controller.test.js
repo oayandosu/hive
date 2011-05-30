af59881a5a7b485c899847732ba245191244fd0a
Ritchie <skawful@gmail.com>
2011-05-30 00:26:10 -0700

    Buttoned up hive.Http

--- test/controller.test.js
+++ test/controller.test.js
@@ -1,7 +1,8 @@
-var vows   = require('vows'),
-	hive   = require('../index'),
-	fs 	   = require('fs'),
-    assert = require('assert');
+var vows     = require('vows'),
+	hive     = require('../index'),
+	fs 	     = require('fs'),
+    assert	 = require('assert'),
+	_		 = require('underscore');
 
 // directory behavior
 vows.describe('controller').addBatch({
@@ -11,17 +12,66 @@ vows.describe('controller').addBatch({
 
 		'has a base route': {
 			topic: function(controller) {
+				controller.get('/', function(req, res) {
+					res.send('ok');
+				});
+				hive.app.listen(3000);
 				var _self = this,
-					test = new hive.Http({path: '/test'});
+					test = new hive.Http({url: 'http://localhost:3000/test'});
 				test.bind('success', function() {
 					_self.callback(null, test);
 				});
 				test.fetch();
 			},
-			
-			'it should respond to a GET request at that route': function(err, response) {
-				
+			'it should respond to a GET request at that route': function(err, test) {
+				assert.equal(test.res.statusCode, 200);
+				assert.equal(test.get('data'), 'ok');
+				//hive.app.close();
 			}
-		}
+		},
+		'has a simple get route': routeMacro(),
+		'has a simple post route': routeMacro({method: 'post'})
     }
 }).export(module);
+
+// macro for route tests
+function routeMacro(opt) {
+	var def = {
+		at: '/test',
+		path: '/',
+		method: 'get',
+		body: 'ok'
+	};
+	opt = _.extend(def, opt);
+	var result = {
+		topic: function(controller) {
+			controller[opt.method](opt.path, function(req, res) {
+				res.send(opt.body);
+			});
+			var _self = this,
+				path = (opt.path === '/') ? '' : opt.path,
+				test = new hive.Http({url: 'http://localhost:3000' + opt.at + path});
+			test.bind('success', function() {
+				_self.callback(null, test);
+			});
+			switch(opt.method) {
+				case 'get':
+				case 'find':
+					test.fetch();
+				break;
+				case 'post':
+					test.set({body: opt.body});
+					test.save();
+				break;
+				case 'delete':
+					test.destroy();
+				break;
+			}
+		}
+	};
+	result['it should respond to a ' + opt.method] = function(err, model) {
+		assert.equal(model.res.statusCode, 200);
+	}
+	return result;
+		
+}
